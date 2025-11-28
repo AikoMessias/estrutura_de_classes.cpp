1d04b0f74914c779a9e5afe0d6ef4452d6f497f4
Danielly Aiko Messias <daniellyaiko2021@gmail.com>
2025-11-28 14:20:05 -0300

    classe_para_mostrar_idade_nome.cpp

---
+++ classe_para_mostrar_idade_nome.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Pessoa {
+public:
+    string nome;
+    int idade;
+    
+    void apresentar() {
+        cout << "oi, eu sou " << nome << " e tenho " << idade << " anos." << endl;
+        
+    }
+};
+
+
+
+int main()
+{
+     Pessoa p;
+     p.nome = "aiko";
+     p.idade = 12;
+     p.apresentar();
+
+       
+
+    return 0;
+}#include <string>
+#include <iostream>
+using namespace std;
+
+class Pessoa {
+public:
+    string nome;
+    int idade;
+    
+    void apresentar() {
+        cout << "oi, eu sou " << nome << " e tenho " << idade << " anos." << endl;
+        
+    }
+};
+
+
+
+int main()
+{
+     Pessoa p;
+     p.nome = "aiko";
+     p.idade = 12;
+     p.apresentar();
+
+       
+
+    return 0;
+}
